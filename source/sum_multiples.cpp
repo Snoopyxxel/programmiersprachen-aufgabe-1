5f9261283d741b6b6ccfb874f3c5ef2247c9d286
Snoopyxxel <snoopyxxel@gmail.com>
2019-04-10 17:18:31 +0200

    added functionality and test for sum_multiples

--- source/sum_multiples.cpp
+++ source/sum_multiples.cpp
@@ -1,9 +1,14 @@
 #include "catch.hpp"
 
 int sum_multiples(){
-    return 1;
+    int  ausg = 0;
+    for(int i = 1; i <= 1000; ++i)
+        if(i % 3 == 0 or i % 5 == 0){
+            ausg += i;
+        }
+    return ausg;
 }
 
 TEST_CASE("sum_multiples"){
-    REQUIRE(sum_multiples() == 1);
+    REQUIRE(sum_multiples() == 234168);
 }

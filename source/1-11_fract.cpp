e3a7514229295deddd98ee40c7bd2302bbf87f44
Snoopyxxel <snoopyxxel@gmail.com>
2019-04-15 15:34:55 +0200

    fract() of negative number now returns negativen Nachkommaanteil

--- source/1-11_fract.cpp
+++ source/1-11_fract.cpp
@@ -1,14 +1,21 @@
 #include "catch.hpp"
 
 float fract(float in){
-    return in - floor(in);
+    if (in < 0) {
+        return floor(in) - in;
+    } else if (in > 0){
+        return in - floor(in);
+    } else{
+        return 0;
+    }
+
 }
 
 TEST_CASE("fract"){
     REQUIRE(fract(4.5) == Approx(0.5));
     REQUIRE(fract(0.5) == Approx(0.5));
-    REQUIRE(fract(-1.5) == Approx(0.5));
-    REQUIRE(fract(-0.5) == Approx(0.5));
+    REQUIRE(fract(-1.5) == Approx(-0.5));
+    REQUIRE(fract(-0.5) == Approx(-0.5));
     REQUIRE(fract(4) == Approx(0));
     REQUIRE(fract(0) == Approx(0));
 }
